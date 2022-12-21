4d2f7ac117642bcfb0e5e5c5fec94758281b6f54
Waqqas <76067933+Waqqas41@users.noreply.github.com>
2022-12-21 18:50:56 -0500

    day one

---
+++ Day_1.java
@@ -0,0 +1,47 @@
+public class Day1 {
+    
+    public Day1(){
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
+/** solution brainstorm and pseudocode:
+basic idea: 
+find the elf with the most calories in his backpack. 
+each elf's inventory is separated by a blank line
+the total calories per elf is the sum of integer values 
+in an elfs inventory 
+//
+
+pseudocode:
+print textfile to console for testing
+input textfile and read through each line
+    while (file value != an empty line){
+    add all integer values separated by a line to an int inventory 
+    }
+
+    inventory = new variable current inventory 
+
+    create a new Node and add it to a linked list every time the while loop breaks 
+
+    if (we reach EOF){
+        return linked list
+        print linked list
+    }
+
+    sort method {
+        
+    }
+
+
+
+
+ */
+
+
+
+
